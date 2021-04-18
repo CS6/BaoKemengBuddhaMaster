ee67d99744e8364b93f98eecdc0af4fc01ef4b68
sddivid <sduum82@gmail.com>
2021-04-18 22:55:49 +0800

    Add files via upload

---
+++ precache-manifest.df549fcf2dc70053bd5c16aac1b36678.js
@@ -0,0 +1,70 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "41e3377d7c0cb50e5625a847c6fe6046",
+    "url": "/index.html"
+  },
+  {
+    "revision": "cb6a02715aee99e5a5df",
+    "url": "/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "068f0a0e4074d0074a66",
+    "url": "/static/js/2.e5f3ecf6.chunk.js"
+  },
+  {
+    "revision": "0b7330e74584067e05b829f48de34b7e",
+    "url": "/static/js/2.e5f3ecf6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "cb6a02715aee99e5a5df",
+    "url": "/static/js/main.60624575.chunk.js"
+  },
+  {
+    "revision": "0d758776d3d5f47638fa",
+    "url": "/static/js/runtime-main.d23c5f3f.js"
+  },
+  {
+    "revision": "1f3ff8441a2f6a7cd8575d110ec7877c",
+    "url": "/static/media/360.1f3ff844.jpg"
+  },
+  {
+    "revision": "cf0899910bf2f4f59eb387d737b63567",
+    "url": "/static/media/AliciaSolid.cf089991.vrm"
+  },
+  {
+    "revision": "223653c5c048927b40e013c82f4220b9",
+    "url": "/static/media/Crouch_To_Stand.223653c5.fbx"
+  },
+  {
+    "revision": "27525540f1ee3c8736c28e993726ec80",
+    "url": "/static/media/Jumping_Down.27525540.fbx"
+  },
+  {
+    "revision": "8678bb27e05b238d7509444936440dce",
+    "url": "/static/media/f15.8678bb27.glb"
+  },
+  {
+    "revision": "e4c7563f840db0bb68eb872efcdf8b98",
+    "url": "/static/media/gundan.e4c7563f.glb"
+  },
+  {
+    "revision": "c0c465d55ef0b8af9047f16af031e272",
+    "url": "/static/media/jk.c0c465d5.vrm"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "bffb7f9d281ea7058005dcc6bcd56123",
+    "url": "/static/media/miku_v2.bffb7f9d.pmd"
+  },
+  {
+    "revision": "ce2964db7f655447e17e827344c70882",
+    "url": "/static/media/wavefile_short.ce2964db.mp3"
+  },
+  {
+    "revision": "f4c8de6a68a56c7191870d4367ac5787",
+    "url": "/static/media/wavefile_v2.f4c8de6a.vmd"
+  }
+]);
